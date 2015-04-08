a5b46a06e4b41f894c79ffd81b69c4fa48d8eba2
gracevente11 <gracevente11@augustana.edu>
2015-04-08 12:30:12 -0500

    added boolean to make sure the velocity is only changed once

--- app/src/main/java/edu/augustana/csc490/soccergame/SoccerBall.java
+++ app/src/main/java/edu/augustana/csc490/soccergame/SoccerBall.java
@@ -8,6 +8,8 @@ public class SoccerBall {
     private float y; // y position
     private float vx; // X velocity
     private float vy; // Y velocity
+    //boolean to make sure that the goalie doesn't "catch" the ball
+    private boolean velocityHasBeenChanged;
 
     //contructor
     public SoccerBall(float x, float y, float vx, float vy){
@@ -15,6 +17,7 @@ public class SoccerBall {
         this.y = y;
         this.vx = vx;
         this.vy = vy;
+        velocityHasBeenChanged = false;
     }
 
     public void moveBall(){
@@ -30,6 +33,10 @@ public class SoccerBall {
     public float getX() { return x; }
 
     public float getVX() { return vx; }
+
+    public boolean getVelocityHasBeenChanged(){ return velocityHasBeenChanged;}
+
+    public void setVelocityHasBeenChangedTrue() { velocityHasBeenChanged = true;}
 
     public void setVX(float newVX) { vx = newVX; }
 }
