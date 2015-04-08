7dcaaf73b1e312ea2b8a119c8e1917606a66fa6e
gracevente11 <gracevente11@augustana.edu>
2015-04-08 12:31:20 -0500

    fixed goalie "catching" the ball issue

--- app/src/main/java/edu/augustana/csc490/soccergame/SoccerShootoutView.java
+++ app/src/main/java/edu/augustana/csc490/soccergame/SoccerShootoutView.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,6 +17,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -37,6 +39,12 @@ implements SurfaceHolder.Callback
     private ArrayList<SoccerBall> soccerBalls; // ArrayList to represent the soccer balls
     private int numberSoccerBalls;
     private Rect goal;
+
+
+    private int goalieLeft;
+    private int goalieTop;
+    private int goalieRight;
+    private int goalieBottom;
     private Rect goalie;
     private int soccerBallRadius;
 
@@ -54,6 +62,8 @@ implements SurfaceHolder.Callback
     private Paint fieldPaint;
 
     private int numberOfGoals;
+
+
 
     private int numGoals;
 
@@ -89,33 +99,15 @@ implements SurfaceHolder.Callback
         screenWidth = w;
         screenHeight = h;
 
-        numberSoccerBalls = 5;
-        numberOfGoals = 0;
-
         goalLeft = screenWidth * 15 / 16;
-        goalTop = screenHeight / 3;
+        goalTop = screenHeight / 4;
         goalRight = screenWidth;
-        goalBottom = screenHeight * 2 / 3;
+        goalBottom = screenHeight * 3 / 4;
 
+        goal = new Rect(goalLeft, goalTop, goalRight, goalBottom);
 
-
-        //Create Soccer Balls
-        Random random = new Random(); //random object to pick random points
         soccerBallRadius = (goalBottom - goalTop)/40; //diameter of the soccerball is 1/20 of the goal
 
-        int left = goalLeft - (goalRight - goalLeft);
-        int top = (goalBottom- (goalBottom-goalTop)/2) - ((goalBottom - goalTop) / 10) ;
-        int right = goalLeft - (goalRight - goalLeft)*3 / 2;
-        int bottom = (goalBottom- (goalBottom-goalTop)/2) + ((goalBottom - goalTop) / 10);
-
-        goalie = new Rect(left, top, right, bottom);// goalie is twice as big as the ball
-
-        for(int i= 1; i<= numberSoccerBalls; i++) {
-            soccerBalls.add(newSoccerBall());
-        }
-
-        // create the goal
-        goal = new Rect(goalLeft, goalTop, goalRight, goalBottom);
         startNewGame();
     }
 
@@ -147,17 +139,33 @@ implements SurfaceHolder.Callback
 
     }
     public void startNewGame() {
+        //clear soccerBalls arraylist
+        for(int i = soccerBalls.size() - 1; i >=0 ; i -- ){
+            soccerBalls.remove(i);
+        }
+
+        goalieLeft = 2 * goalLeft - goalRight;
+        goalieTop = (goalBottom- (goalBottom-goalTop)/2) - ((goalBottom - goalTop) / 10) ;
+        goalieRight = goalLeft - (goalRight - goalLeft) / 3;
+        goalieBottom = (goalBottom- (goalBottom-goalTop)/2) + ((goalBottom - goalTop) / 10);
+        goalie = new Rect(goalieLeft, goalieTop, goalieRight, goalieBottom);// goalie is twice as big as the ball?
+
+        numberSoccerBalls = 5;
+        numberOfGoals = 0;
+
+        for(int i= 1; i<= numberSoccerBalls; i++) {
+            soccerBalls.add(newSoccerBall());
+        }
+
         if(gameOver) {
             soccerShootoutThread = new SoccerShootoutThread(getHolder());
             soccerShootoutThread.start();
+            gameOver = false;
         }
     }
 
     //up date where the soccer balls are and check if they are any goals or balls that have gone off of the screen
     public void updateGame(){
-        //for (SoccerBall ball: soccerBalls){
-          // ball.moveBall();
-        //}
 
         for (int i = 0; i<soccerBalls.size(); i ++){
             soccerBalls.get(i).moveBall();
@@ -165,7 +173,7 @@ implements SurfaceHolder.Callback
             //check if the ball is in the goal
             if (soccerBalls.get(i).getX() >= goalLeft && soccerBalls.get(i).getX() < goalLeft+1 && soccerBalls.get(i).getY() >= goalTop && soccerBalls.get(i).getY() <= goalBottom){
                 numberOfGoals ++;
-                Log.d("numGoals", "" + numberOfGoals);
+                Log.d(TAG, "numGoals: " + numberOfGoals);
             }
             //check if a ball goes off of the screen
             if(soccerBalls.get(i).getX() > screenWidth || soccerBalls.get(i).getX() <= -1){
@@ -174,18 +182,18 @@ implements SurfaceHolder.Callback
             }
 
             // check if a ball hits the goalie
-            if (goalie.contains( (int)soccerBalls.get(i).getX(), (int) soccerBalls.get(i).getY())) {
+            if (goalie.contains( (int)soccerBalls.get(i).getX(), (int) soccerBalls.get(i).getY()) && !soccerBalls.get(i).getVelocityHasBeenChanged()) {
                 //change the velocity to opposite x direction
                 soccerBalls.get(i).setVX(-1 * soccerBalls.get(i).getVX());
-            }
-
-
-            if ( numberOfGoals >= 10){
-                soccerShootoutThread.setRunning(false);
-                showGameOverDialog();
-                gameOver = true;
+                soccerBalls.get(i).setVelocityHasBeenChangedTrue();
             }
         }
+        if ( numberOfGoals >= 10){
+            soccerShootoutThread.setRunning(false);
+            gameOver = true;
+            showGameOverDialog();
+        }
+
     }
     public void updateView (Canvas canvas){
         if (canvas != null) {
@@ -212,6 +220,10 @@ implements SurfaceHolder.Callback
                         AlertDialog.Builder builder =
                                 new AlertDialog.Builder(getActivity());
                         builder.setTitle("Game Over");
+                        builder.setMessage("Shots taken: " + "\nShots blocked: " + "\nPlay again?");
+                        builder.setNegativeButton("No.", endGameClickListener);
+                        builder.setPositiveButton("Yes.", playAgainClickListener);
+
                      return builder.create();
                     }
                 };
@@ -226,6 +238,23 @@ implements SurfaceHolder.Callback
                 }
         );
     }
+    DialogInterface.OnClickListener endGameClickListener = new DialogInterface.OnClickListener() {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            mainActivity.finish();
+        }
+    };
+
+    DialogInterface.OnClickListener playAgainClickListener = new DialogInterface.OnClickListener() {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+            startNewGame();
+        }
+    };
+
 
 
     @Override
@@ -246,16 +275,12 @@ implements SurfaceHolder.Callback
         // gets location of user touch
         Point touchPoint = new Point ((int) event.getX(), (int) event.getY());
 
-        int left = touchPoint.x - ((goalRight - goalLeft) / 4);
-        int top = touchPoint.y - ((goalBottom - goalTop) / 10) ;
-        int right = touchPoint.x + ((goalRight - goalLeft) / 4);;
-        int bottom = touchPoint.y + ((goalBottom - goalTop) / 10);
-
-        goalie.set(left, top, right, bottom);
+        goalieLeft = touchPoint.x - ((goalRight - goalLeft) / 3);
+        goalieTop = touchPoint.y - ((goalBottom - goalTop) / 10) ;
+        goalieRight = touchPoint.x + ((goalRight - goalLeft) / 3);
+        goalieBottom = touchPoint.y + ((goalBottom - goalTop) / 10);
+        goalie.set(goalieLeft, goalieTop, goalieRight, goalieBottom);
     }
-
-
-
 
     public SoccerBall newSoccerBall(){
         Random random = new Random(); //random object to pick random points
@@ -279,6 +304,7 @@ implements SurfaceHolder.Callback
         private boolean threadIsRunning = true;
 
         public SoccerShootoutThread (SurfaceHolder holder) {
+            Log.w(TAG, "created Thread");
             surfaceHolder = holder;
             setName("SoccerShootoutThread");
         }
@@ -315,6 +341,7 @@ implements SurfaceHolder.Callback
                         surfaceHolder.unlockCanvasAndPost(canvas);
                 }
             }
+            Log.w(TAG, "Thread finished");
         }
 }
 }
